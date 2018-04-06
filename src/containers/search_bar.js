427592c42a51e6e961d7c1ae94e4222467bcc930
Pedro Acharon III <pacharon@apollo.com.ph>
2018-04-06 20:04:56 +0800

    [UPDATE]: updated search bar form to only include bottom button

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -28,8 +28,8 @@ class SearchBar extends Component {
 
     render() {
         return(
-            <form onSubmit = { this.onFormSubmit } >
-            <div className="main_search pt-4 pl-5 pr-5 pb-5">
+
+            <div className="main_search pt-4 pl-5 pr-5 pb-2">
               <div className="row clearfix">
                 <div className="mx-auto"><h3>Find a 7-Eleven Store</h3></div>
                 <div className="col-12 pl-0 pr-0">
@@ -42,22 +42,23 @@ class SearchBar extends Component {
               </div>
               <div className="row pt-3 clearfix">
                 <div className="col-12 pr-0 pl-0">
-                  <div className="form-label-group">
-                    <input id="store_search"
-                        className="pt-4 form-control"
-                        placeholder="Enter Province or City"
-                        autoFocus=""
-                        type="text"
-                        onChange={this.onInputChange}
-                        value= { this.state.term }
-                        />
-                    <label htmlFor="store_search">Enter Province or City</label>
-                  </div>
-                  <button className="btn mt-2 btn-lg btn-success w-100"><i className="fas fa-search"></i> &nbsp; Find Stores</button>
+                    <form onSubmit = { this.onFormSubmit } >
+                        <div className="form-label-group">
+                        <input id="store_search"
+                            className="pt-4 form-control"
+                            placeholder="Enter Province or City"
+                            autoFocus=""
+                            type="text"
+                            onChange={this.onInputChange}
+                            value= { this.state.term }
+                            />
+                            <label htmlFor="store_search">Enter Province or City</label>
+                        </div>
+                        <button className="btn mt-2 btn-lg btn-success w-100"><i className="fas fa-search"></i> &nbsp; Find Stores</button>
+                    </form>
                 </div>
               </div>
             </div>
-            </form>
         );
     }
 }
