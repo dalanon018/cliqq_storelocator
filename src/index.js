cfae43ef203572876859655bb8df3014ec95cbb4
Pedro Acharon III <pacharon@apollo.com.ph>
2018-05-11 19:43:29 +0800

    [UPDATE]: added redux logger

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
 import reducers from './reducers'
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore)
+import logger from 'redux-logger';
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise, logger)(createStore)
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
